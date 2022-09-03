11ef7b6b106905d3aab59c52754ef225547000a8
mollikacomputer <mollikacomputer3@gmail.com>
2022-09-04 00:09:02 +0600

    ejs dynamic id sent to static file

--- index.js
+++ index.js
@@ -9,14 +9,19 @@ const userRouter = require('./routes/v1/users.router');
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+app.set('view engin', 'ejs');
+
 // app.use(express.text()); // for sent to text
 
 // app.use('/user', userRouter);
 app.use('/api/v1/users', userRouter);
 
 app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/public/test.html');
+    // res.sendFile(__dirname + '/public/test.html');
     // res.send('Test app');
+    res.render("home.ejs", {
+        id:5,
+    })
 })
 app.listen(port, () => {
     console.log(` listening to port ${port}`,);
